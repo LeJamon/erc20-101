2f8eb2ab11813a52e8feff40d0dc476cbceb83e8
Hussenet Thomas <Lejamon.c@protonmail.com>
2022-11-26 15:08:45 +0100

    All exercice done

--- contracts/XpGzb.sol
+++ contracts/XpGzb.sol
@@ -1,21 +1,56 @@
 pragma solidity ^0.6.2;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./IExerciceSolution.sol";
+contract XpGzb is IExerciceSolution, ERC20 {
 
-contract XpGzb is ERC20 {
-
-    mapping (address=>uint8) AllowedUser;
+    mapping (address=>bool) AllowedUser;
+    mapping (address=>uint8) TierListUser;
    
-    //constructor() ERC20("XpGzb", "XpGzb") public {
-       // _mint(address(this), 594727662000000000000000000);
-    //}
-
-    constructor(string memory name, string memory symbol,uint256 initialSupply) public ERC20(name, symbol) {
-        _mint(msg.sender, initialSupply);
-       
+    constructor(string memory name, string memory symbol, uint initialSupply) ERC20(name, symbol) public {
+        _mint(address(this), initialSupply);
+        AllowedUser[address(0x7C5629d850eCD1E640b1572bC0d4ac5210b38FA5)]=true; 
+        TierListUser[address(0x7C5629d850eCD1E640b1572bC0d4ac5210b38FA5)]=2; 
     }
 
-    function getToken() public{
+    function symbol() public  view  override(IExerciceSolution,ERC20) returns (string memory){
+        return("7bZpW");
+        // problème de ticker , résolu en rappelant la fonction
+    }
+    
+    function getToken() external override returns (bool){
+        require(AllowedUser[msg.sender],"you are not whitelisted"); 
+        _transfer(address(this),msg.sender,1000); // moins de gas fee
+        return true; 
+    }
+
+    function buyToken() external override payable returns (bool){ 
+
+        require(AllowedUser[msg.sender], "You are not whitelisted");
+        require(msg.value>0,"you are not paying");
+        if(TierListUser[msg.sender]==1){
+        _transfer(address(this),msg.sender,msg.value*10);
+        }
+        if(TierListUser[msg.sender]==2){
+        _transfer(address(this),msg.sender,msg.value*20);
+        }
+        return true; 
+    }
+
+    function isCustomerWhiteListed(address customerAddress) external override returns (bool){
+        return AllowedUser[customerAddress];
+    }
+
+    function customerTierLevel(address customerAddress) external  override returns (uint256){
+        return TierListUser[customerAddress];
+    }
+
+    /*function symbol() external view override returns (string memory){
+        return ("XpGzb");
+    }
+    
+
+    function getToken() external override return(bool){
         _transfer(address(this),msg.sender,1000); // moins de gas fee 
     }
 
--- migrations/2_td_deploy.js
+++ migrations/2_td_deploy.js
@@ -1,5 +1,22 @@
-const erc20 = artifacts.require("XpGzb.sol"); 
+const Str = require('@supercharge/strings')
+// const BigNumber = require('bignumber.js');
 
-module.exports = function(deployer, network, accounts){
-    deployer.deploy(erc20);  
+var TDToken = artifacts.require("XpGzb.sol");
+
+
+module.exports = (deployer, network, accounts) => {
+    deployer.then(async () => {
+        await deployErc20(deployer, network, accounts); 
+        await deployRecap(deployer, network, accounts); 
+    });
+};
+
+async function deployErc20(deployer, network, accounts) {
+	Q2contract = await TDToken.new("7bZpW","7bZpW",web3.utils.toBN("581502946000000000000000000"))
 }
+
+async function deployRecap(deployer, network, accounts) {
+	console.log("7bZpW : " + Q2contract.address)
+}
+
+//Nb : truffle migrate --f 2 --to 2 --network goerli
