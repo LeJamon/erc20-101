8d1e7946603f6ee14cd8aa2425cef3f09a739f96
Lejamon <lejamon.c@protonmail.com>
2022-11-26 13:14:39 +0100

    deploy modifier

--- migrations/2_td_deploy.js
+++ migrations/2_td_deploy.js
@@ -1,25 +1,5 @@
-const Str = require('@supercharge/strings')
-// const BigNumber = require('bignumber.js');
+const erc20 = artifacts.require("XpGzb.sol"); 
 
-var XpGzb = artifacts.require("XpGzb.sol");
-
-module.exports = (deployer, network, accounts) => {
-    deployer.then(async () => {
-        await deployXpGzb(deployer, network, accounts); 
-        await deployRecap(deployer, network, accounts); 
-    });
-};
-
-async function deployXpGzb(deployer, network, accounts) {
-	XpGzb = await XpGzb.new("XpGzb","XpGzb",web3.utils.toBN("594727662000000000000000000")); 
-    deployer.deploy(XpGzb)
+module.exports = function(deployer, network, accounts){
+    deployer.deploy(erc20);  
 }
-
-
-async function deployRecap(deployer, network, accounts) {
-	console.log("XpGzb " + XpGzb.address)
-}
-
-
-// truffle run verify ERC20TD@0x09f14a40Fd672B5B056FF8b5c343498452CAC4b2 --network goerli
-// truffle run verify evaluator@0x7C5629d850eCD1E640b1572bC0d4ac5210b38FA5 --network goerli
